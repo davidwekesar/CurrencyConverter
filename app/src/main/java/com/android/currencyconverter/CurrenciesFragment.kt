c49a55c23eb76dac1f99f3450f38086e7a4fc4a2
davidwekesar <davidwekesar@gmail.com>
2021-07-21 08:54:34 +0300

    Add Currencies Fragment and setup Navigation
    Add item layout resource for currencies recyclerview

---
+++ app/src/main/java/com/android/currencyconverter/CurrenciesFragment.kt
@@ -0,0 +1,18 @@
+package com.android.currencyconverter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CurrenciesFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
